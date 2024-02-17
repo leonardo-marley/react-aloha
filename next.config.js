f878c46981f39098154f294aa2c20fc39a870a19
leonardo-marley <111132131+leonardo-marley@users.noreply.github.com>
2024-02-17 17:43:45 -0300

    UPD - Novo Deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
+    domains: ['localhost','https://drive.google.com'],
     formats:['image/webp','image/avif']
   },
   reactStrictMode: true,
